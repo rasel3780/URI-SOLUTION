ef96091ef447e8b36d2b476430ec88e34005b5b9
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:19:25 +0600

    Average 1

---
+++ 1005.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,media;
+    cin>>a>>b;
+    media = ((a*3.5)+(b*7.5))/11;
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<media<<"\n";
+    return 0;
+}
