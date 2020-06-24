7db9373234b44d393d12cb4604b6d565b8911c2f
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:26:59 +0600

    Salary

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,w;
+    float s;
+    cin>>n>>w>>s;
+    printf("NUMBER = %d\n",n);
+    printf("SALARY = U$ %.2f\n",(s*w));
+    return 0;
+}
+
