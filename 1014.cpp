1226987efb4abc9588bbe1819cefd882717e393c
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:17:17 +0600

    Consumption

---
+++ 1014.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    double y, res;
+
+    cin>>x>>y;
+    res = x/y;
+    cout<<fixed<<setprecision(3)<<res<<" km/l\n";
+
+    return 0;
+}
