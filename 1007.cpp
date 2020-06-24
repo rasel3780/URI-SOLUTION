1151f47626c2be35f16ab0c46975d3de0b5db297
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:27:50 +0600

    Difference

---
+++ 1007.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,dif;
+    cin>>a>>b>>c>>d;
+    dif=(a*b)-(c*d);
+    cout<<"DIFERENCA = "<<dif<<"\n";
+    return 0;
+}
