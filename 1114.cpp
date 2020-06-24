b35ee264896fdd1abd77b1121909ec979919367e
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 14:38:53 +0600

    Fixed Password

---
+++ 1114.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int pass;
+    while(1)
+    {
+        cin>>pass;
+        if(pass == 2002)
+        {
+            cout<<"Acesso Permitido\n";
+            break;
+        }
+        else
+        {
+            cout<<"Senha Invalida\n";
+        }
+    }
+    return 0;
+}
+
