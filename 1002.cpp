fb2de23ad7c8d783f855def8eb562bf9f2d23351
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:21:12 +0600

    Area of a Circle

---
+++ 1002.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define n 3.14159
+using namespace std;
+int main()
+{
+    double r,a;
+    cin>>r;
+    a=n*r*r;
+    cout<<"A="<<fixed<<setprecision(4)<<a<<"\n";
+    return 0;
+}
