55113d04917f8ea4760dc74f66765e82810defd3
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 13:49:45 +0600

    Sequence IJ 2

---
+++ 1096.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1; i<=9; i=i+2)
+    {
+        cout<<"I="<<i<<" "<<"J=7"<<"\n";
+        cout<<"I="<<i<<" "<<"J=6"<<"\n";
+        cout<<"I="<<i<<" "<<"J=5"<<"\n";
+    }
+    return 0;
+}
+
