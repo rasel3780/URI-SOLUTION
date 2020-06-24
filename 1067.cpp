c01af5ad8be4fb3f4a800c732c656bd56dd14274
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-24 15:06:30 +0600

    Odd Numbers

---
+++ 1067.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    for (int i = 1; i <= x; i += 2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
