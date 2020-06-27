0cf02f849a91fdbdb97981c14b127a06ee8605a9
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-28 03:08:45 +0600

    Back to High School Physics

---
+++ 1197.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int v, t, displacement;
+    while(cin>>v>>t)
+    {
+        displacement = v*t*2;
+        cout<<displacement<<"\n";
+    }
+    return 0;
+}
