85896f971ce8cb9f5ac1f70945a64465a117be3d
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-28 01:13:01 +0600

    Ho Ho Ho

---
+++ 1759.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        cout<<"Ho";
+        if(n>0)
+        {
+            cout<<" ";
+        }
+    }
+    cout<<"!\n";
+    return 0;
+}
