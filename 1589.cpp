b8dff7d938e67e7c1c56f51f973d09341104e64e
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-06-28 01:05:26 +0600

    Bob Conduit

---
+++ 1589.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, r1, r2, r3;
+    cin>>t;
+    while(t--)
+    {
+        cin>>r1>>r2;
+        r3 = r1+r2;
+        cout<<r3<<"\n";
+    }
+    return 0;
+}
