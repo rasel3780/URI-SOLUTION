ead3d532848dfd57de878c549ba5ccb711a0dfb9
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-02 19:56:14 +0600

    Vitória and Her Indecision

---
+++ 1924.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    string s;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        getline(cin, s);
+    }
+    getchar();
+    cout<<"Ciencia da Computacao\n";
+    return 0;
+}
