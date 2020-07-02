164205ec3ae6b0b213c6ee9c831d65abbea4b6c7
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-02 09:54:10 +0600

    Bill

---
+++ 1866.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int c, n;
+    cin>>c;
+    while(c--)
+    {
+        cin>>n;
+        if(n%2==0)
+        {
+            cout<<"0\n";
+        }
+        else
+        {
+            cout<<"1\n";
+        }
+    }
+    return 0;
+}
