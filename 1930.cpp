de509872eef1028f13ebe9d78851c41e41f69005
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-02 20:59:46 +0600

    Electrical Outlet

---
+++ 1930.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t1, t2, t3, t4, sum;
+
+    cin>>t1>>t2>>t3>>t4;
+    sum = (t1-1) + (t2-1) + (t3-1) + t4;
+    cout<<sum<<"\n";
+
+    return 0;
+}
