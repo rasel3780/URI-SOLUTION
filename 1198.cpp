724e4b6b36f4d0f718fcfda158a9c1fc0717f314
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-03 10:09:32 +0600

    Hashmat the Brave Warrior

---
+++ 1198.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long x, y, z;
+    while(cin>>x>>y)
+    {
+        z=x-y;
+        cout<<abs(z)<<"\n";
+    }
+    return 0;
+}
