6fa5f68e76e60f1620d422007af2d9e599fc626b
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-04 17:44:10 +0600

    Name at Form

---
+++ 2160.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin, str);
+    if(str.length()>80)
+    {
+        cout<<"NO\n";
+    }
+    else
+    {
+        cout<<"YES\n";
+    }
+    return 0;
+}
