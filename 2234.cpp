02f19280774524781f0288d187a544fc191a34ac
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-06 01:09:26 +0600

    Hot Dogs

---
+++ 2234.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  rasel kibria
+ *    created: 05.07.2020
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double m, n, avg;
+
+    cin>>m>>n;
+    avg = m/n;
+    cout<<fixed<<setprecision(2)<<avg<<"\n";
+
+    return 0;
+}
