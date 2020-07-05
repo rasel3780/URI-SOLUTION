1001a7fe07b75934c8e852d7138f2180f6a75375
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-06 00:54:52 +0600

    Divisors I

---
+++ 1157.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author:  rasel kibria
+ *    created: 05.07.2020
+**/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i;
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<"\n";
+        }
+    }
+
+
+    return 0;
+}
