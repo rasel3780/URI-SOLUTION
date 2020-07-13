4088c406d0ff90252f2f8aa26dd49aeb0ff3afce
RASEL KIBRIA <43618232+rasel3780@users.noreply.github.com>
2020-07-13 20:11:21 +0600

    I am Toorg!

---
+++ 2581.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    int n, i;
+
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin.ignore();
+        getline(cin, s);
+    }
+    for(i=0; i<n; i++)
+    {
+        cout<<"I am Toorg!\n";
+    }
+    return 0;
+}
